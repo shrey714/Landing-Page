c4d622829c887dd42d3bac1c38df2464170eb4ab
shrey <pshrey0000@gmail.com>
2024-07-28 17:54:38 +0530

    home page started

---
+++ src/components/landingPage/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="fixed top-0 navbar p-0 min-h-6 h-6 bg-white justify-center">
+      <div className="navbar-center">
+        <p className="text-base font-medium text-gray-800">DardiBook</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
