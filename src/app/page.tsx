ad02ac773c25e090354f05add4c47007ef5a9650
shrey <pshrey0000@gmail.com>
2024-08-05 21:17:41 +0530

    marq

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
     <main className="min-h-screen w-full">
       <Header />
       <HeroSection />
-      <MarqueeSection />
+      {/* <MarqueeSection /> */}
       <FeaturesSection />
       <MiddleBanner />
       <ImageWithTextSection />
