b62532c9d3a5884f5c34233e7f97901e44a2ab4b
shrey <pshrey0000@gmail.com>
2024-08-05 13:10:55 +0530

    contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,14 +1,39 @@
-import React from "react";
-
+"use client";
+import React, { useState } from "react";
 const ContactUsForm = () => {
+  const [loading, setLoading] = useState(true);
+
+  const handleLoad = () => {
+    setLoading(false);
+  };
+
   return (
-    <iframe
-      aria-label="Contact Us"
-      frameBorder="0"
-      style={{ width: "100%", border: "none", minHeight: "100vh" }}
-      scrolling="no"
-      src="https://forms.zohopublic.in/dardibook/form/ContactUs/formperma/1lNqF48RvRdXaE0Zoq_ICd6DQM9WWESKwRcH28vYHwI"
-    ></iframe>
+    <>
+      {loading && (
+        <div
+          style={{
+            width: "100%",
+            height: "100%",
+            display: "flex",
+            padding: "50px 0 50px 0",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <span className="loading loading-spinner"></span>
+        </div>
+      )}
+      <div style={{ position: "relative", minHeight: "100vh" }}>
+        <iframe
+          aria-label="Contact Us"
+          frameBorder="0"
+          style={{ width: "100%", border: "none", minHeight: "100vh" }}
+          scrolling="no"
+          src="https://forms.zohopublic.in/dardibook/form/ContactUs/formperma/1lNqF48RvRdXaE0Zoq_ICd6DQM9WWESKwRcH28vYHwI"
+          onLoad={handleLoad}
+        ></iframe>
+      </div>
+    </>
   );
 };
 
