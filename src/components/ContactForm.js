41431ea35b302343928eb33e004feefc140be127
shrey <pshrey0000@gmail.com>
2024-08-05 12:19:44 +0530

    contact form

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,33 +1,8 @@
-"use client";
-import React, { useEffect, useRef } from "react";
+import React from "react";
 
 const ContactUsForm = () => {
-  const iframeRef = useRef(null);
-
-  const resizeIframe = () => {
-    const iframe = iframeRef.current;
-    if (iframe) {
-      console.log("height===", iframe.contentWindow.document.body.scrollHeight);
-      iframe.style.height =
-        iframe.contentWindow.document.body.scrollHeight + "px";
-    }
-  };
-
-  useEffect(() => {
-    const iframe = iframeRef.current;
-    if (iframe) {
-      iframe.addEventListener("load", resizeIframe);
-      console.log("usefeffeffe")
-      // Cleanup the event listener on unmount
-      return () => {
-        iframe.removeEventListener("load", resizeIframe);
-      };
-    }
-  }, []);
-
   return (
     <iframe
-      ref={iframeRef}
       aria-label="Contact Us"
       frameBorder="0"
       style={{ width: "100%", border: "none", minHeight: "100vh" }}
