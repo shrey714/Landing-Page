f21008277888b76bc4fc515b07f95d284b6b44b8
Shrey Patel <76216765+shrey714@users.noreply.github.com>
2024-08-22 14:55:37 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ import PricingPlans from "@/components/landingPage/PricingPlans";
 
 export default function Home() {
   return (
-    <main className="min-h-screen w-full carouselMainElement">
+    <main className="min-h-screen w-full">
       <Header />
       <HeroSection />
       {/* <MarqueeSection /> */}
