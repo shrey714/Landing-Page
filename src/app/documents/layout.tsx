e6c5a77d26d44021c3b9c7961e423e657d8bf18e
shrey <pshrey0000@gmail.com>
2024-10-28 19:14:45 +0530

    navbar animation added

--- src/app/documents/layout.tsx
+++ src/app/documents/layout.tsx
@@ -1,11 +1,14 @@
 import { ReactNode } from "react";
-import HeaderDocument from "@/components/HeaderDocument";
 import FooterDocuments from "@/components/FooterDocuments";
+import dynamic from "next/dynamic";
 
 export default function RootLayout({ children }: { children?: ReactNode }) {
+  const Header = dynamic(() => import("@/components/HeaderDocument"), {
+    ssr: false, // This ensures the component is not SSR'd
+  });
   return (
     <div className="pt-24 min-h-dvh bg-gray-300">
-      <HeaderDocument />
+      <Header />
       {children}
       <FooterDocuments />
     </div>
