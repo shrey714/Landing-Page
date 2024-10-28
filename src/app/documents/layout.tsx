518a54840b64b6928742b8c3474242c0245f23b7
shrey <pshrey0000@gmail.com>
2024-10-28 19:52:45 +0530

    navbar animation performance

--- src/app/documents/layout.tsx
+++ src/app/documents/layout.tsx
@@ -2,10 +2,11 @@ import { ReactNode } from "react";
 import FooterDocuments from "@/components/FooterDocuments";
 import dynamic from "next/dynamic";
 
+const Header = dynamic(() => import("@/components/HeaderDocument"), {
+  ssr: false, // This ensures the component is not SSR'd
+});
+
 export default function RootLayout({ children }: { children?: ReactNode }) {
-  const Header = dynamic(() => import("@/components/HeaderDocument"), {
-    ssr: false, // This ensures the component is not SSR'd
-  });
   return (
     <div className="pt-24 min-h-dvh bg-gray-300">
       <Header />
